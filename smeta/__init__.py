378e9564dd20678d3ef3935f35aa092d1fe37cdd
Simon Lundström <simlu+github@su.se>
2026-03-13 14:06:42 +0100

    Make sure Flask logger is the default one
    Also read logging config from LOGGING in config file

--- smeta/__init__.py
+++ smeta/__init__.py
@@ -1,6 +1,7 @@
-import logging
+import logging.config
 
 from flask import Flask
+from flask.logging import default_handler
 
 app = Flask(__name__)
 
@@ -9,6 +10,16 @@ try:
 except (FileNotFoundError, RuntimeError) as ex:
     logging.error("Couldn't read config file: %s", ex)
 app.config.from_prefixed_env()
+
+logging.config.dictConfig({**{"version": 1}, **app.config.get("LOGGING", {})})
+
+# Set Flasks logging handler to all loggers and enable them all
+for logger in [
+    logging.getLogger(name)
+    for name in logging.root.manager.loggerDict  # pylint: disable=no-member
+]:
+    logger.handlers = [default_handler]
+    logger.disabled = False
 
 
 @app.route("/")
