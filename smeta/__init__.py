41e9e5d46b5ae66a9246da0a50de9e735b3dcd48
Simon Lundström <simlu+github@su.se>
2026-03-13 14:01:13 +0100

    Config from file in FLASK_CONFIG or env vars

--- smeta/__init__.py
+++ smeta/__init__.py
@@ -1,6 +1,14 @@
+import logging
+
 from flask import Flask
 
 app = Flask(__name__)
+
+try:
+    app.config.from_envvar("FLASK_CONFIG", silent=False)
+except (FileNotFoundError, RuntimeError) as ex:
+    logging.error("Couldn't read config file: %s", ex)
+app.config.from_prefixed_env()
 
 
 @app.route("/")
